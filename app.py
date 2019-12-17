4501e32c867728880fbb336e5036c763184853b6
mpolensek <matevz.polensek@aviatnet.com>
2019-12-17 08:47:30 +0100

    Added lecture 16 - Jinja.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import time
 
 # Documentation is like sex.
 # When it's good, it's very good.
@@ -11,17 +12,27 @@ app = Flask(__name__)
 @app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html")
+    time_now = time.time()
+    return render_template("index.html", trenutni_cas=time_now)
 
 
 @app.route("/about-me")
 def about_me():
-    return render_template("about_me.html")
+    user = "Kostanj"
+    return render_template("about_me.html", user=user)
 
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template("portfolio.html")
+    projects = [
+        {"name": "Test",
+         "year": 2018},
+        {"name": "Fitnes",
+         "year": 2014},
+        {"name": "Carpark",
+         "year": 2017}
+    ]
+    return render_template("portfolio.html", projects=projects)
 
 
 if __name__ == '__main__':
