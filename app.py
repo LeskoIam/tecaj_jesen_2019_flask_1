adde24fe8ee170dc2209abab274f497c3674ad0e
mpolensek <matevz.polensek@aviatnet.com>
2020-01-07 10:22:06 +0100

    Added lecture 17 - HTTP requests & Cookies.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, make_response
 import time
 
 # Documentation is like sex.
@@ -9,20 +9,31 @@ import time
 app = Flask(__name__)
 
 
-@app.route("/index")
-@app.route("/")
+@app.route("/index", methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
     time_now = time.time()
-    return render_template("index.html", trenutni_cas=time_now)
+    user_name = request.cookies.get("name")
+    return render_template("index.html", trenutni_cas=time_now, name=user_name)
 
 
-@app.route("/about-me")
+@app.route("/about-me", methods=["GET", "POST"])
 def about_me():
-    user = "Kostanj"
-    return render_template("about_me.html", user=user)
+    if request.method == "GET":
+        user_name = request.cookies.get("name")
+        return render_template("about_me.html", name=user_name)
+    elif request.method == "POST":
+        user_name = request.form.get("name")
+        print(user_name)
+        print(request.form.get("email"))
+        print(request.form.get("comment"))
+
+        response = make_response(redirect("index"))
+        response.set_cookie("name", user_name)
+        return response
 
 
-@app.route("/portfolio")
+@app.route("/portfolio", methods=["GET"])
 def portfolio():
     projects = [
         {"name": "Test",
@@ -32,7 +43,8 @@ def portfolio():
         {"name": "Carpark",
          "year": 2017}
     ]
-    return render_template("portfolio.html", projects=projects)
+    user_name = request.cookies.get("name")
+    return render_template("portfolio.html", projects=projects, name=user_name)
 
 
 if __name__ == '__main__':
